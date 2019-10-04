7a3ddfc41e6289f7e615bfceb171daa9050ad05a
tomkent321 <tomkent321@yahoo.com>
2019-10-03 22:12:05 -0600

    convert search to hooks

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,58 +1,52 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-export class Search extends Component {
-  state = {
-    text: ''
-  };
+const Search = ({ searchUsers, clearForm, users, setAlert }) => {
+  const [text, setText] = useState('');
 
-  static propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    users: PropTypes.array.isRequired,
-    clearForm: PropTypes.func.isRequired,
-    setAlert: PropTypes.func.isRequired
-  };
+  const onChange = e => setText(e.target.value);
 
-  onChange = e => this.setState({ [e.target.name]: e.target.value });
-
-  onSubmit = e => {
+  const onSubmit = e => {
     e.preventDefault();
-    if (this.state.text === '') {
-      this.props.setAlert('Please enter something', 'light');
+    if (text === '') {
+      setAlert('Please enter something', 'light');
     } else {
-      this.props.searchUsers(this.state.text);
-      this.setState({ text: '' });
+      searchUsers(text);
+      setText('');
     }
   };
 
-  render() {
-    const { users, clearForm } = this.props;
+  return (
+    <div>
+      <form className="form" onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="text"
+          placeholder="Search Users..."
+          value={text}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
 
-    return (
-      <div>
-        <form className="form" onSubmit={this.onSubmit}>
-          <input
-            type="text"
-            name="text"
-            placeholder="Search Users..."
-            value={this.state.text}
-            onChange={this.onChange}
-          />
-          <input
-            type="submit"
-            value="Search"
-            className="btn btn-dark btn-block"
-          />
-        </form>
+      {users.length > 0 && (
+        <button className="btn btn-light btn-block" onClick={clearForm}>
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
 
-        {users.length > 0 && (
-          <button className="btn btn-light btn-block" onClick={clearForm}>
-            Clear
-          </button>
-        )}
-      </div>
-    );
-  }
-}
+Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  users: PropTypes.array.isRequired,
+  clearForm: PropTypes.func.isRequired,
+  setAlert: PropTypes.func.isRequired
+};
 
 export default Search;
